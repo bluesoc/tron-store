de0ccb81a2512d2157b8ec8be5cfa2a7468ef058
bluesoc <145991084+bluesoc@users.noreply.github.com>
2026-01-13 23:02:45 -0300

    Prevent default click action

--- frontend/src/Components/StoreSection.tsx
+++ frontend/src/Components/StoreSection.tsx
@@ -76,6 +76,10 @@ function classNames(...classes: string[]) {
 export default function TStoreSectionail() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
 
+const noClick = (e: any) => {
+        e.preventDefault();
+    }
+
   return (
     <div className="bg-white">
       <div>
@@ -106,7 +110,7 @@ export default function TStoreSectionail() {
               {/* Filters */}
               <form className="mt-4 border-t border-gray-200">
                 <h3 className="sr-only">Categories</h3>
-                <ul role="list" className="px-2 py-3 font-medium text-gray-900">
+                <ul role="list" onClick={noClick} className="px-2 py-3 font-medium text-gray-900">
                   {subCategories.map((category) => (
                     <li key={category.name}>
                       <a href={category.href} className="block px-2 py-3">
@@ -241,7 +245,7 @@ export default function TStoreSectionail() {
               {/* Filters */}
               <form className="hidden lg:block">
                 <h3 className="sr-only">Categories</h3>
-                <ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
+                <ul role="list" onClick={noClick} className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
                   {subCategories.map((category) => (
                     <li key={category.name}>
                       <a href={category.href}>{category.name}</a>
